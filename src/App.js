c8f2c1786e438e1cfc8e1e82e3081a4f893e7fe4
bruhoffman <bruhoffman@gmail.com>
2024-03-13 14:15:04 -0300

    Exercícios da Aula

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
 import TelaPrincipal from "./components/TelaPrincipal/TelaPrincipal.js";
+import { useState } from "react";
 
 const GlobalStyled = createGlobalStyle`
   html {
@@ -13,22 +14,34 @@ const GlobalStyled = createGlobalStyle`
     margin: 0;
     box-sizing: border-box;
   }
-`
+`;
 const MainContainer = styled.main`
   height: 100vh;
-`
+`;
 
 function App() {
+  const [tela, setTela] = useState("1");
+
+  const trocarTela = (tela) => {
+    switch (tela) {
+      case "login":
+        return <TelaLogin tela={tela} setTela={setTela} />;
+
+      case "cadastro":
+        return <TelaCadastro tela={tela} setTela={setTela} />;
+
+      case "principal":
+        return <TelaPrincipal tela={tela} setTela={setTela} />;
+
+      default:
+        return <TelaLogin tela={tela} setTela={setTela} />;
+    }
+  };
 
   return (
-    <MainContainer >
+    <MainContainer>
       <GlobalStyled />
-
-      <TelaLogin />
-
-      <TelaCadastro />
-      
-      <TelaPrincipal />
+      {trocarTela(tela)}
     </MainContainer>
   );
 }
